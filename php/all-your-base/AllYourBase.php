84d69d30b41548145bfb6c874f7b48815543a72f
Hawk Dykes <hawkwdykes@gmail.com>
2025-09-07 16:15:00 -0500

    remove unneeded guard

--- php/all-your-base/AllYourBase.php
+++ php/all-your-base/AllYourBase.php
@@ -16,11 +16,6 @@ function rebase(int $fromBase, array $digits, int $toBase): array
         throw new InvalidArgumentException('all digits must satisfy 0 <= d < input base');
     }
 
-    if (!count($digits) || !count(array_filter($digits, fn ($d) => $d > 0))) {
-        return [0];
-    }
-
-    // calculate the actual value to operate on (using input array values as digits in base $fromBase)
     $reversed = array_reverse($digits);
 
     $value = 0;
