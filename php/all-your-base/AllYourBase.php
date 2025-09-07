d3bfa2a42cdd07672cf040d32a676cd0532ee89f
Hawk Dykes <hawkwdykes@gmail.com>
2025-09-07 15:52:33 -0500

    clever modulo trick

--- php/all-your-base/AllYourBase.php
+++ php/all-your-base/AllYourBase.php
@@ -16,9 +16,9 @@ function rebase(int $fromBase, array $digits, int $toBase): array
         throw new InvalidArgumentException('all digits must satisfy 0 <= d < input base');
     }
 
-    if (!count($digits) || !count(array_filter($digits, fn ($d) => $d > 0))) {
-        return [0];
-    }
+    // if (!count($digits) || !count(array_filter($digits, fn ($d) => $d > 0))) {
+    //     return [0];
+    // }
 
     // calculate the actual value to operate on (using input array values as digits in base $fromBase)
     $reversed = array_reverse($digits);
@@ -28,29 +28,10 @@ function rebase(int $fromBase, array $digits, int $toBase): array
         $value += $reversed[$i] * ($fromBase ** $i);
     }
 
-    // find the largest power of toBase needed (this will be the length of the output array)
-    $largestPower = 0;
-    while ($value >= $toBase ** $largestPower && $value >= $toBase ** ($largestPower + 1)) {
-        ++$largestPower;
-    }
+    do {
+		$output[] = $value % $toBase;
+		$value /= $toBase;
+	} while ($value > 0);
 
-    // find values for each position in the new base
-    $remainingValue = $value;
-    for ($pow = $largestPower; $pow >= 0; $pow--) {
-        $multiplier = 0;
-
-        $valToTest = fn ($mult) => ($toBase ** $pow) * $mult;
-
-        while (
-            $valToTest($multiplier) <= $remainingValue
-            && $valToTest($multiplier + 1) <= $remainingValue
-        ) {
-            ++$multiplier;
-        }
-
-        $remainingValue -= (($toBase ** $pow) * $multiplier);
-        $output[] = $multiplier;
-    }
-
-    return $output;
+	return array_reverse($output);
 }
