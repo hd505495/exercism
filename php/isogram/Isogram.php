52a1f82735202c70e5967f0cda61142a9057250a
Hawk Dykes <hawkwdykes@gmail.com>
2025-09-14 15:47:30 -0500

    improved solution - test unique-d string against original string

--- php/isogram/Isogram.php
+++ php/isogram/Isogram.php
@@ -4,20 +4,7 @@ declare(strict_types=1);
 
 function isIsogram(string $word): bool
 {
-    $freq = [];
-    $chars = str_split($word);
+    $chars = str_split(preg_replace('/[\s-]/', '', strtolower($word)));
 
-    for ($i = 0; $i < count($chars); $i++) {
-        if (ctype_alpha($chars[$i])) {
-            $char = strtolower($chars[$i]);
-
-            if (array_key_exists($char, $freq)) {
-                ++$freq[$char];
-            } else {
-                $freq[$char] = 1;
-            }
-        }
-    }
-
-    return count(array_filter($freq, fn ($val) => $val > 1)) === 0;
+    return array_unique($chars) === $chars;
 }
